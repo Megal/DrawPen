8358d70d28ec5246bfab4b6baf97c586b718844b
Svyatoshenko "Megal" Misha <m1sha.odesk@gmail.com>
2017-03-08 23:29:21 +0300

    Initial Commit

---
+++ DrawPen/DrawPen.h
@@ -0,0 +1,19 @@
+//
+//  DrawPen.h
+//  DrawPen
+//
+//  Created by Svyatoshenko "Megal" Misha on 2017-03-08.
+//  Copyright © 2017 Megal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DrawPen.
+FOUNDATION_EXPORT double DrawPenVersionNumber;
+
+//! Project version string for DrawPen.
+FOUNDATION_EXPORT const unsigned char DrawPenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawPen/PublicHeader.h>
+
+
